e1c52dd15ceaa76bc02a0334fbc0dec28ca661e2
Nanang Dwi Febrianto <nanang.febrianto.nf@gmail.com>
2023-01-12 21:55:33 +0700

    mencoba menambah cart

---
+++ client/src/main/java/com/example/model/cartpem.java
@@ -0,0 +1,28 @@
+package com.example.model;
+
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+public class cartpem {
+
+    private Map<String, CartEntry> entries;
+    public  cartpem() {
+        this.entries = new HashMap<>();
+    }
+    public void addProduct(String productName){
+        CartEntry producEntry = entries.get(productName.toUpperCase());
+        if(producEntry != null){
+            producEntry.increaseQuantity();
+        }else{
+            Menu product = Menu.valueOf(productName)
+        }
+    }
+    public void removeProduct(String productName){
+        entries.get(productName.toUpperCase())
+    }
+    public double calculateTotal(){
+        float total = 0;
+        for(Car)
+    }
+}
